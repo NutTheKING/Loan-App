1efd110e55326c0ff711f6638a6c5c56ed5ae89e
NutTheKind <tinutchan@gmail.com>
2026-08-05 11:06:33 +0700

    deploy: prepare Firebase web and Render API

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,14 +1,18 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConfig {
   static const _compiledApiBaseUrl = String.fromEnvironment('API_BASE_URL');
 
   static String get apiBaseUrl {
+    final platformDefault = kIsWeb
+        ? '${Uri.base.origin}/api/v1'
+        : 'http://localhost:4000/api/v1';
     final configuredUrl = _compiledApiBaseUrl.isNotEmpty
         ? _compiledApiBaseUrl
         : dotenv.env['API_BASE_URL'] ??
               dotenv.env['base_url'] ??
-              'http://localhost:4000/api/v1';
+              platformDefault;
     return configuredUrl.replaceFirst(RegExp(r'/+$'), '');
   }
 }
